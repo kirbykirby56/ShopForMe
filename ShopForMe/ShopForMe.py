1a696354f8513f6cbc4ca19f2b75e749a6280b3f
Kirbykirby56 <emeraldspire@outlook.com>
2018-07-29 09:09:27 -0400

    Changes to ShopForMe.py to make it more concise and output less repetitive information.

--- ShopForMe/ShopForMe.py
+++ ShopForMe/ShopForMe.py
@@ -4,29 +4,27 @@ import re
 class GoogleSpider(scrapy.Spider):
     name = 'googlespider'
     output = False;
+
+
     def __init__(self, searchTerms='', log=False, **kwargs):
         output = log
-        searchTerms.replace(" ", "+");
+        searchTerms.replace(" ", "+")#It's probably true that one of these does all of the work, but I have both so that it's guaranteed to work anyway.
         re.sub(' ', "+", searchTerms)
         self.start_urls = [("http://www.google.com/search?q=" + searchTerms + "&tbm=shop")] #Open google.com/search?<SEARCH+TERMS>&tbm=shop, the shopping tab for that search.
         if self.output:
+            print("Start Urls: ")
             print(self.start_urls)
+            print("\n")
+
+
     def parse(self, response):
         if self.output:
-            print("\n" + response.url + "\n")
-        #print(response.body)
-        resList = response.css("#ires > ol > div > div > div").extract()#.extract_first())
-        if self.output:
-            for item in resList:
-                print(item)
-                print("\n")
+            print(response.url + "\n")
         for item in response.css("#ires > ol > div"):
             name = item.css("div > a > img::attr(alt)").extract_first()
             price = item.css("div:nth-child(2) > div > b::text").extract_first()
-            link = item.css("div > a::attr(href)").extract_first()
-            link = "https://www.google.com" + link
+            link = "https://www.google.com" + item.css("div > a::attr(href)").extract_first()
             re.sub("&amp", "&", link)
-            
             if self.output:
                 print("NAME: " + name)
                 print("LINK: " + link)
@@ -36,11 +34,8 @@ class GoogleSpider(scrapy.Spider):
                 'Price': price,
                 'Link': link
             }
-        nex = response.css("#foot > table > tr > td:last_child > a > span:last_child::text").extract_first()
-        if nex == "Next":
+        if response.css("#foot > table > tr > td:last_child > a > span:last_child::text").extract_first() == "Next": #This will run until there's no "next" button.
             next_page_url = str("http://www.google.com" + str(response.css("#foot > table > tr > td:last_child > a::attr(href)").extract_first()));
-            if self.output:
-                print(next_page_url)
         else :
             next_page_url = None
         if next_page_url is not None and next_page_url is not "http://www.google.com":
