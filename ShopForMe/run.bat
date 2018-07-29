6ec67cbf31377fd2367fd7f98601b7caf2d835f2
Kirbykirby56 <emeraldspire@outlook.com>
2018-07-29 08:51:57 -0400

    Added messages in batch because there's no output from Scrapy

--- ShopForMe/run.bat
+++ ShopForMe/run.bat
@@ -5,7 +5,10 @@ REM echo %searchterms%
 set searchterms2 = !searchterms: =%%20!
 REM echo %searchterms: =+%
 set /P file="What file to output? | "
+echo Running spider...
 scrapy runspider "ShopForMe.py" -a searchTerms="%searchTerms: =+%" -a log=False -o"%file%" --nolog
+echo Done! Further processing output...
 Line_DeDuper.exe %file%
 Html_Tag_Remover %file%
+echo Done!
 pause
