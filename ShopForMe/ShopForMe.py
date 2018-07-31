23287cbf94010a6086a0515d7bdf4d5a7cd0ff3f
Kirbykirby56 <emeraldspire@outlook.com>
2018-07-31 11:47:48 -0400

    Much further condensed code, using one-line if statements.

--- ShopForMe/ShopForMe.py
+++ ShopForMe/ShopForMe.py
@@ -9,28 +9,20 @@ class GoogleSpider(scrapy.Spider):
         searchTerms.replace(" ", "+")#It's probably true that one of these does all of the work, but I have both so that it's guaranteed to work anyway.
         re.sub(' ', "+", searchTerms)
         self.start_urls = [("http://www.google.com/search?q=" + searchTerms + "&tbm=shop")] #Open google.com/search?<SEARCH+TERMS>&tbm=shop, the shopping tab for that search.
-        if self.output:
-            print("Start Urls: ")
-            print(self.start_urls)
+        print("Start Urls: " + self.start_urls[0]) if self.output else None
             
     def parse(self, response):
-        if self.output:
-            print(response.url)
+        print(response.url) if self.output else None
         for item in response.css("#ires > ol > div"):
             name = item.css("div > a > img::attr(alt)").extract_first()
             price = item.css("div:nth-child(2) > div > b::text").extract_first()
             link = "https://www.google.com" + item.css("div > a::attr(href)").extract_first()
             re.sub("&amp", "&", link)
-            if self.output:
-                print("    NAME: " + name + "\n    LINK: " + link + "    PRICE: " + price + "\n")
+            print("    NAME: " + name + "\n    LINK: " + link + "    PRICE: " + price + "\n" if self.output else "")
             yield {
                 'Name': name,
                 'Price': price,
                 'Link': link
             }
-        if response.css("#foot > table > tr > td:last_child > a > span:last_child::text").extract_first() == "Next": #This will run until there's no "next" button.
-            next_page_url = str("http://www.google.com" + str(response.css("#foot > table > tr > td:last_child > a::attr(href)").extract_first()));
-        else :
-            next_page_url = None
-        if next_page_url is not None and next_page_url is not "http://www.google.com":
-            yield scrapy.Request(response.urljoin(next_page_url))
+            next_page_url = str("http://www.google.com" + str(response.css("#foot > table > tr > td:last_child > a::attr(href)").extract_first())) if response.css("#foot > table > tr > td:last_child > a > span:last_child::text").extract_first() == "Next" else None #If there's a "Next" button, it's the link it points to, else nothing.
+            yield scrapy.Request(response.urljoin(next_page_url)) if next_page_url is not None else None
